fd6acde2ad6eb7e96fce3f4520b908889e2514e1
Remi Ferrand <remi.mathieu.ferrand@gmail.com>
2014-03-21 22:25:32 +0100

    Fail as soon as a test fails to run

--- run_tests.sh
+++ run_tests.sh
@@ -4,4 +4,10 @@
 
 NOOP=${NOOP:"--noop"}
 
-find ./tests -type f -name '*.pp' -exec puppet apply --modulepath="$(pwd)/.." ${NOOP} --verbose {} \;
+modpath="$(pwd)/.."
+for t in $(find ./tests -type f -name '*.pp')
+do
+	puppet apply --modulepath=${modpath} --verbose ${NOOP} ${t}
+	[[ $? -ne 0 ]] && { echo "FAIL: '${t}'" >&2; exit 1; }
+
+done
