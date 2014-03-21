d874fed03b951ff85e8eac89412194b2a1f3a4f9
Remi Ferrand <remi.mathieu.ferrand@gmail.com>
2014-03-21 22:36:23 +0100

    Remove NOOP var and force --noop

--- run_tests.sh
+++ run_tests.sh
@@ -2,12 +2,9 @@
 
 [[ ! -d "./tests" ]] && { echo "No tests to run"; exit 0; }
 
-NOOP=${NOOP:"--noop"}
-
 modpath="$(pwd)/.."
 for t in $(find ./tests -type f -name '*.pp')
 do
-	puppet apply --modulepath=${modpath} --verbose ${NOOP} ${t}
-	[[ $? -ne 0 ]] && { echo "FAIL: '${t}'" >&2; exit 1; }
-
+    puppet apply --modulepath=${modpath} --verbose --noop ${t}
+    [[ $? -ne 0 ]] && { echo "FAIL: '${t}'" >&2; exit 1; }
 done
