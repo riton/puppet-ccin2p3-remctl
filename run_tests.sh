e5a897b5eaa566024cce0f88f89948c2d2ffae87
Remi Ferrand <remi.mathieu.ferrand@gmail.com>
2014-03-21 21:58:46 +0100

    Add travis configuration file and add modulepath option to run tests Run tests as root for Augeas resources would failed. Now handle TCP and UDP resources in /etc/services files

--- run_tests.sh
+++ run_tests.sh
@@ -1,5 +1,19 @@
-#!/bin/sh
+#!/bin/bash
+
+set -x
 
 [[ ! -d "./tests" ]] && { echo "No tests to run"; exit 0; }
 
-find ./tests -type f -name '*.pp' -exec puppet apply --noop --verbose {} \;
+old_modpath=$(puppet config print modulepath)
+modpath="${old_modpath}:$(pwd)/.."
+for t in $(find ./tests -type f -name '*.pp')
+do
+    echo "[*] -- ${t} --"
+    puppet apply --modulepath=${modpath} --verbose --debug --noop ${t}
+    [[ $? -ne 0 ]] && { echo "FAIL: '${t}'" >&2; exit 2; }
+    echo "[*] -- ${t} passed --"
+done
+
+echo "[*] All tests passed"
+
+exit 0
