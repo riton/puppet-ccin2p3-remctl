0ce8cc64c62d10cbd825c559c408ffb1ef158307
Remi Ferrand <remi.mathieu.ferrand@gmail.com>
2014-03-21 21:58:46 +0100

    Add travis configuration file and add modulepath option to run tests

--- run_tests.sh
+++ run_tests.sh
@@ -1,5 +1,5 @@
-#!/bin/sh
+#!/bin/bash
 
 [[ ! -d "./tests" ]] && { echo "No tests to run"; exit 0; }
 
-find ./tests -type f -name '*.pp' -exec puppet apply --noop --verbose {} \;
+find ./tests -type f -name '*.pp' -exec puppet apply --modulepath="$(pwd)/.." --noop --verbose {} \;
