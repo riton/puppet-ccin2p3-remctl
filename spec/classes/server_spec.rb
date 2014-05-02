18c354fe379cee68bd82c23685bc5208f37e75c6
Remi Ferrand <remi.ferrand@cc.in2p3.fr>
2014-05-02 16:37:06 +0200

    Add test suite for remctl::server class.
    ```
    $ /usr/bin/ruby -S rspec spec/classes/server_spec.rb --color
    .....................................................
    
    Finished in 7.66 seconds
    53 examples, 0 failures
    ```

--- spec/classes/server_spec.rb
+++ spec/classes/server_spec.rb
@@ -98,6 +98,10 @@ describe 'remctl::server', :type => :class do
                         }).that_comes_before("Augeas[#{augeas_name}]")
                     end
 
+                    it 'should modify /etc/services before xinetd service declarated' do
+                        should contain_augeas(augeas_name).that_comes_before("Xinetd::Service[#{xservice_name}]")
+                    end
+
                 end # context with default values
 
                 context 'with bad value' do
@@ -113,7 +117,7 @@ describe 'remctl::server', :type => :class do
 
                 end # context with bad value
 
-                context 'with custom value' do
+                context 'with ensure absent' do
                     let :params do {
                         :ensure     => 'absent'
                     } end
@@ -154,33 +158,6 @@ describe 'remctl::server', :type => :class do
                         end
                     end
                 end # describe 'interaction with #manage_user' do
-
-                describe 'interaction with #manage_group' do
-
-                    context 'with ensure absent' do
-                        let :params do {
-                            :ensure         => 'absent',
-                            :manage_group   => true,
-                            :group          => 'somegroup'
-                        } end
-                        
-                        it 'should have group absent' do
-                            should contain_group('somegroup').with_ensure('absent')
-                        end
-                    end
-
-                    context 'with ensure present' do
-                        let :params do {
-                            :ensure         => 'present',
-                            :manage_group   => true,
-                            :group          => 'somegroup'
-                        } end
-                        
-                        it 'should have group present' do
-                            should contain_group('somegroup').with_ensure('present')
-                        end
-                    end
-                end # describe 'interaction with #manage_group' do
 
             end # describe #ensure
 
@@ -426,7 +403,7 @@ describe 'remctl::server', :type => :class do
 
             end # describe #port
 
-            describe '#user / #manage_user' do
+            describe '#user / #group / #manage_user' do
 
                 context 'with default values' do
                     let :params do {
@@ -436,9 +413,13 @@ describe 'remctl::server', :type => :class do
                         should_not contain_user(default_user)
                     end
 
+                    it 'should NOT have group' do
+                        should_not contain_group(default_group)
+                    end
+
                 end # context with default values
 
-                context 'with bad user value' do
+                context 'with bad user value for user' do
                     let :params do {
                         :user   => false,
                     } end
@@ -450,6 +431,19 @@ describe 'remctl::server', :type => :class do
                     end
 
                 end # context with bad user value
+
+                context 'with bad group value for group' do
+                    let :params do {
+                        :group   => false,
+                    } end
+
+                    it 'should raise error' do
+                        expect {
+                            should compile
+                        }.to raise_error(Puppet::Error, / is not a string/)
+                    end
+
+                end # context with bad group value
 
                 context 'with bad manage_user value' do
                     let :params do {
@@ -464,21 +458,202 @@ describe 'remctl::server', :type => :class do
 
                 end # context with manage_user value
 
-                context 'with custom value' do
+                context 'with custom values' do
                     let :params do {
                         :manage_user    => true,
-                        :user           => 'username'
+                        :user           => 'username',
+                        :group          => 'groupname'
                     } end
 
                     it 'should have user' do
-                        should contain_user('username')
+                        should contain_user('username').that_notifies("Package[#{package_name}]")
+                        should contain_user('username').that_requires('Group[groupname]')
                     end
 
-                    it 'should not work when user is root'
+                    it 'should have group' do
+                        should contain_group('groupname')
+                    end
 
                 end # context with custom value
 
-            end # describe #user / #group
+                context 'with custom value root' do
+                    
+                    let :params do {
+                        :manage_user    => true,
+                        :user           => 'root',
+                        :group           => 'root',
+                    } end
+
+                    it 'should not have user' do
+                        should_not contain_user('root')
+                    end
+
+                    it 'should not have group' do
+                        should_not contain_group('root')
+                    end
+
+                end # context 'with custom value root'
+
+                context 'with custom value zero' do
+                    
+                    let :params do {
+                        :manage_user    => true,
+                        :user           => 0,
+                        :group           => 0
+                    } end
+
+                    it 'should not have user' do
+                        should_not contain_user(0)
+                    end
+
+                    it 'should not have group' do
+                        should_not contain_group(0)
+                    end
+
+                end # context 'with custom value zero'
+
+                context 'with non-root user and root group' do
+                    let :params do {
+                        :manage_user    => true,
+                        :user           => 'username',
+                        :group          => 'root'
+                    } end
+
+                    it 'should have user with empty require' do
+                        should contain_user('username').with({
+                            :require    => nil
+                        })
+                    end
+
+                end # context 'with user root and non-root group'
+
+            end # describe #user / #manage_user
+
+            describe '#only_from' do
+
+                context 'with default values' do
+                    let :params do {
+                    } end
+
+                    it 'should have default' do
+                        should contain_xinetd__service(xservice_name).with({
+                            :only_from      => [ '0.0.0.0' ]
+                        })
+                    end
+
+                end # context with default values
+
+                context 'with bad value' do
+                    let :params do {
+                        :only_from  => 'somestring'
+                    } end
+
+                    it 'should raise error' do
+                        expect {
+                            should compile
+                        }.to raise_error(Puppet::Error, / is not an Array/)
+                    end
+
+                end # context with bad value
+
+                context 'with custom value' do
+                    let :params do {
+                        :only_from  => [ '134.158.69.34', '127.0.0.1' ]
+                    } end
+
+                    it 'should have passed only from' do
+                        should contain_xinetd__service(xservice_name).with({
+                            :only_from  => /^134.158.69.34 127.0.0.1$/
+                        })
+                    end
+
+                end # context with custom value
+
+            end # describe #only_from
+
+            describe '#no_access' do
+
+                context 'with default values' do
+                    let :params do {
+                    } end
+
+                    it 'should have default' do
+                        should contain_xinetd__service(xservice_name).with({
+                            :no_access      => nil
+                        })
+                    end
+
+                end # context with default values
+
+                context 'with bad value' do
+                    let :params do {
+                        :no_access  => 'somestring'
+                    } end
+
+                    it 'should raise error' do
+                        expect {
+                            should compile
+                        }.to raise_error(Puppet::Error, / is not an Array/)
+                    end
+
+                end # context with bad value
+
+                context 'with custom value' do
+                    let :params do {
+                        :no_access  => [ '134.158.69.34', '127.0.0.1' ]
+                    } end
+
+                    it 'should have passed no access' do
+                        should contain_xinetd__service(xservice_name).with({
+                            :no_access  => /^134.158.69.34 127.0.0.1$/
+                        })
+                    end
+
+                end # context with custom value
+
+            end # describe #no_access
+
+            describe '#bind' do
+
+                context 'with default values' do
+                    let :params do {
+                    } end
+
+                    it 'should have default' do
+                        should contain_xinetd__service(xservice_name).with({
+                            :bind   => nil
+                        })
+                    end
+
+                end # context with default values
+
+                context 'with bad value' do
+                    let :params do {
+                        :bind   => false
+                    } end
+
+                    it 'should raise error' do
+                        expect {
+                            should compile
+                        }.to raise_error(Puppet::Error, / is not a string/)
+                    end
+
+                end # context with bad value
+
+                context 'with custom value' do
+                    let :params do {
+                        :bind   => '134.158.69.34'
+                    } end
+
+                    it 'should have passed bind' do
+                        should contain_xinetd__service(xservice_name).with({
+                            :bind   => '134.158.69.34'
+                        })
+                    end
+
+                end # context with custom value
+
+            end # describe #bind
 
         end # describe running on
 
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,6 +5,10 @@ require 'rubygems'
 require 'rspec-puppet'
 require 'puppetlabs_spec_helper/module_spec_helper'
 
+# RSpec.configure do |c|
+#     c.filter_run :focus => true
+# end
+
 @common_specs = {
     :basedir                    => '/etc/remctl',
     :confdir                    => '/etc/remctl/conf.d',
