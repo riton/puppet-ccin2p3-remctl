e15989f97419fc456226c44c1d98ce439475e0bc
Remi Ferrand <remi.ferrand@cc.in2p3.fr>
2014-08-20 11:16:16 +0200

    Fix puppetlabs-concat usage to become compliant with v1.0.4.
    * puppetlabs-concat v1.0.4 is the latest version supported by puppetlabs.
    * This module was using `ensure` parameter on the `concat` type even if this parameter is not yet available in v1.0.4 and puppet-remctl Modulefile pretends that puppetlabs-concat v1.x is compatible.
    * With no possibility to `ensure` that the final concatenated file is _absent_, when no command is defined, an empty file with only _WARNINGS_ is left on the host (i.e there is no way to actually really remove an empty command file)

---
+++ tests/server/99_command_absent.pp
@@ -0,0 +1,50 @@
+
+class {'remctl::server': }
+
+$ensure = 'absent'
+
+remctl::server::command { 'my_test_command':
+    command             => 'command_name',
+    subcommand          => 'ALL',
+    executable          => '/bin/test',
+    acls                => ['princ:test@TEST.REALM.ORG']
+}
+
+remctl::server::command { 'kadmin_cpw':
+    ensure          => $ensure,
+    command         => 'kadmin',
+    subcommand      => 'change_password',
+    executable      => '/usr/sbin/kadmin',
+    options         => {
+        help      => '--help',
+        'summary' => '--summary',
+    },
+    acls            => ['ANYUSER']
+}
+
+remctl::server::command { 'kadmin_lock':
+    ensure          => $ensure,
+    command         => 'kadmin',
+    subcommand      => 'lock_user',
+    executable      => '/usr/sbin/kadmin',
+    options         => {
+        'help'      => '--help',
+        'summary'   => '--summary',
+    },
+    acls            => ['princ:admin@EXAMPLE.ORG']
+}
+
+remctl::server::command { 'kadmin_aa':
+    ensure          => $ensure,
+    command         => 'kadmin',
+    subcommand      => '00aasomehting',
+    executable      => '/usr/sbin/kadmin',
+    options         => {
+        'help'      => '--help',
+        'summary'   => '--summary',
+    },
+    acls            => ['princ:where@EXAMPLE.ORG']
+}
+
+
+# vim: tabstop=4 shiftwidth=4 softtabstop=4
